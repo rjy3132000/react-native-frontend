2af0e6f11fbc5e5b6ef8c2c66a281060381b5b95
yrajeshtechsierra <rajesh.yadav@techsierra.in>
2024-03-23 13:21:37 +0530

    CRUD SCreen Input Fields

--- Screens/CRUD.js
+++ Screens/CRUD.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const Login = () => {
   const navigation = useNavigation();
   return (
     <View>
-      <Text>Login</Text>
-      <Button
+      <Text>Welcome to Add Notes</Text>
+      <View>
+        <Text>Enter First Name</Text>
+        <TextInput placeholder="Enter First Name" />
+      </View>
+      <View>
+        <Text>Enter Second Name</Text>
+        <TextInput placeholder="Enter Second Name" />
+      </View>
+
+      {/* <Button
         title="Back to Home"
         onPress={() => navigation.navigate("Home")}
-      />
+      /> */}
     </View>
   );
 };
